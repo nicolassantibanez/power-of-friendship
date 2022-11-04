9d9c358a711f8c46fa3334a4999406c5183378c6
Nicolás Santibáñez <nsantibanezb@gmail.com>
2022-11-03 22:09:31 -0300

    feat: Double-tap to shoot!

--- scripts/basic_projectile.gd
+++ scripts/basic_projectile.gd
@@ -1,16 +1,24 @@
 extends Area2D
 
-var SPEED = 300
+var SPEED = 600
 
 var move_direction = Vector2(0,0)
 
 
 func _ready():
 	connect("body_entered", self, "_on_body_entered")
-	
+
+
+func _process(delta):
+	position += SPEED * move_direction * delta
+
+
 func on_timer_timeot():
-	queue_free()
-	
+	pass
+#	queue_free()
+
+
 func _on_body_entered(body: Node):
 	# Por ahora al chocar desaparece xd
-	queue_free()
+#	queue_free()
+	pass
--- scripts/player_wasd.gd
+++ scripts/player_wasd.gd
@@ -1,29 +1,58 @@
 extends KinematicBody2D
 
-var SPEED = 200
-var ACCELERATION = 1000
+const DOUBLE_PRESS_TIMEOUT = 0.3 # Timeout between key presses
+const MAX_KEY_PRESSES = 2
 
-var projectile = preload("res://scenes/basic_projectile.tscn")
+var SPEED = 300
+var ACCELERATION = 10000
+
+var projectile_path = preload("res://scenes/basic_projectile.tscn")
 var velocity = Vector2()
+var last_key_delta = 0    # Time since last keypress
+var key_combo = []        # Current combo
 
-onready var projectile_spanw = $projectileSpawn
-# var a = 2
-# var b = "text"
+onready var projectile_spawn = $projectileSpawn
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
 	var move_input = Vector2(
 		Input.get_axis("wasd_left", "wasd_right"),
 		Input.get_axis("wasd_up", "wasd_down")
 	).normalized()
 	
 	velocity = velocity.move_toward(move_input * SPEED, ACCELERATION * delta)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+	last_key_delta += delta
+
+
+func _input(event):
+	if event is InputEventKey and event.pressed and !event.echo: # If distinct key press down
+		if last_key_delta > DOUBLE_PRESS_TIMEOUT:                   # Reset combo if stale
+			key_combo = []
+	
+		key_combo.append(event.scancode)   
+		if key_combo.size() > MAX_KEY_PRESSES:               # Prune if necessary
+			key_combo.pop_front()
+
+		if len(key_combo) == MAX_KEY_PRESSES and key_combo[0] == key_combo[1]:
+			if event.is_action_pressed("wasd_left"):
+				attack(Vector2(-1, 0))
+			elif event.is_action_pressed("wasd_right"):
+				attack(Vector2(1, 0))
+			elif event.is_action_pressed("wasd_up"):
+				attack(Vector2(0, -1))
+			elif event.is_action_pressed("wasd_down"):
+				attack(Vector2(0, 1))
+		
+		print(key_combo) # Log the combo (could pass to combo evaluator)
+		last_key_delta = 0      # Reset keypress timer
+
+func attack(direction:Vector2):
+	projectile_spawn.look_at(direction + global_position)
+	var projectile = projectile_path.instance()
+	get_parent().add_child(projectile)
+	projectile.position = $projectileSpawn/Sprite.global_position
+	print(projectile.position)
+	projectile.move_direction = direction
+	print(direction)
